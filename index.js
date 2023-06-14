21b3a423d20f8dc899f6317aa1fefa3aad99b644
Vishal Rai <85121812+eviltwin7648@users.noreply.github.com>
2023-06-14 12:48:15 +0530

    changed the casing

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function pickUserHand(hand) {
   let results = document.querySelector(".result");
   results.style.display = "flex";
 
-  if (hand == "rock") {
+  if (hand == "Rock") {
     document.getElementById("userpickimg").src = "./assets/Rock.png";
-  } else if (hand == "paper") {
+  } else if (hand == "Paper") {
     document.getElementById("userpickimg").src = "./assets/Paper.png";
   } else {
     document.getElementById("userpickimg").src = "./assets/Scissors.png";
@@ -25,26 +25,26 @@ function pickComputerhand() {
 }
 let score = 0;
 function refree(hand, cpHand) {
-  if (hand === "rock" && cpHand === "scissors") {
+  if (hand === "Rock" && cpHand === "Scissors") {
     document.getElementById("decision").textContent = "YOU WIN";
     score =  score+1;
-  } else if (hand === "rock" && cpHand === "paper") {
+  } else if (hand === "Rock" && cpHand === "Paper") {
     document.getElementById("decision").textContent = "YOU LOSE";
-  } else if (hand === "rock" && cpHand === "rock") {
+  } else if (hand === "Rock" && cpHand === "Rock") {
     document.getElementById("decision").textContent = "it's a tie!";
-  } else if (hand === "paper" && cpHand === "scissors") {
+  } else if (hand === "Paper" && cpHand === "Scissors") {
     document.getElementById("decision").textContent = "YOU LOSE";
-  } else if (hand === "paper" && cpHand === "paper") {
+  } else if (hand === "Paper" && cpHand === "Paper") {
     document.getElementById("decision").textContent = "it's a tie!";
-  } else if (hand === "paper" && cpHand === "rock") {
+  } else if (hand === "Paper" && cpHand === "Rock") {
     document.getElementById("decision").textContent = "YOU WIN";
     score =  score+1;
-  } else if (hand === "scissors" && cpHand === "scissors") {
+  } else if (hand === "Scissors" && cpHand === "Scissors") {
     document.getElementById("decision").textContent = "it's a tie!";
-  } else if (hand === "scissors" && cpHand === "paper") {
+  } else if (hand === "Scissors" && cpHand === "Paper") {
     document.getElementById("decision").textContent = "YOU WIN";
     score =  score+1;
-  } else if (hand === "scissors" && cpHand === "rock") {
+  } else if (hand === "Scissors" && cpHand === "Rock") {
     document.getElementById("decision").textContent = "YOU LOSE";
   }
   document.querySelector(".score h1").textContent = score;
